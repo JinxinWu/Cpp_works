02006a18dcea8d80059dcd0ddcb7abb26003da29
JinxinWu <103267221+JinxinWu@users.noreply.github.com>
2022-06-14 10:52:43 +0800

    Rename 源.cpp to 试卷例题4.cpp

--- 试卷例题4.cpp
+++ 试卷例题4.cpp
@@ -1,7 +1,7 @@
-/*ģ�� STL stack ��ģ��ӿڣ����ʵ�� CStack ��ģ�壬�����������ջ����ջ����ջ
-�ա�ȡջ��Ԫ�صȹ��ܣ���֧�ֿ������ơ����ⲻ��ʹ�� STL stack��������ʹ�� STL ��
-��������������ʹ�� CStack ��ģ��Ĳ��Գ��������������н����*/
-//��ʾջ���ݺ���ģ�嶨��
+/*模拟 STL stack 类模板接口，设计实现 CStack 类模板，该类需具有入栈、出栈、判栈
+空、取栈顶元素等功能，并支持拷贝控制。本题不可使用 STL stack，但可以使用 STL 其
+它容器。下面是使用 CStack 类模板的测试程序和输入输出运行结果。*/
+//显示栈内容函数模板定义
 #include <iostream>
 #include <deque>
 using namespace std;
@@ -39,7 +39,7 @@ void CStack<T>::pop()
 template <class T>
 void Dump(ostream& os, CStack<T>& S)
 {
-	while (!S.empty())//�൱��!S.empty (),��Ҫ���ڲ�����Ԫ
+	while (!S.empty())//相当于!S.empty (),主要用于测试友元
 	{
 		os << S.top() << " ";
 		S.pop();
@@ -49,20 +49,20 @@ void Dump(ostream& os, CStack<T>& S)
 int main()
 {
 	int n, i;
-	cin >> n; //����ָ������
-	CStack<string> S1, S2; //���� T ģ����� 2 ��ջ����
+	cin >> n; //读入指令数量
+	CStack<string> S1, S2; //建立 T 模板类的 2 个栈对象
 	for (i = 0; i < n; i++)
 	{
 		int v;
 		string x;
-		//ִ������ÿ��ָ��
+		//执行输入每条指令
 		cin >> v >> x;
 		if (v == 1)
 			S1.push(x);
 		else
 			S2.push(x);
 	}
-	//��ʾջ����
+	//显示栈内容
 	Dump(cout, S1);
 	Dump(cout, S2);
 }
